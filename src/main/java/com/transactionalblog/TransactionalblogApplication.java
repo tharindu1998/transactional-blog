10772d2179c53972741d1a7d466025cb23bb2314
tharindu1998 <tharindudulshanfdo@gmail.com>
2024-07-05 13:14:00 +0530

    initial

---
+++ src/main/java/com/transactionalblog/TransactionalblogApplication.java
@@ -0,0 +1,13 @@
+package com.transactionalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transactionalblog/TransactionalblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
